2cca9ac9cbe4cc2f54ce361a8d5eba8e340c385d
Keri Melich <keri@nobl9.com>
2022-02-16 12:48:14 -0700

    fmt

--- modules/aws/nobl9/main.tf
+++ modules/aws/nobl9/main.tf
@@ -8,7 +8,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "encryption_rules"
   bucket = aws_s3_bucket.nobl9_exporter_bucket.bucket
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm     = "AES256"
+      sse_algorithm = "AES256"
     }
   }
 }
